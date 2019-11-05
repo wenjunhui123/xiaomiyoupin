caa335a1663fc4799c6ad3a5173224e9b9f44b3b
wangkangli <528787754@qq.com>
2019-11-05 23:28:04 +0800

    分类基本样式

--- xiaomi/src/pages/Classify/Classify.vue
+++ xiaomi/src/pages/Classify/Classify.vue
@@ -1,52 +1,50 @@
 <template>
   <div class="container">
+    <div class="top">
+        <div class="inp">
+            <a class="icon">
+                <img src="../../assets/images/home_search.png" alt="">
+            </a>
+            <span>年度爆款限时5折购</span>
+        </div>
+        
+    </div>
+    
     <div class="g-body">
-                <div class="nav">
-                    <ul>
-                        <li class="current">有品推荐</li>
-                        <li class="">居家</li>
-                        <li class="">家电</li>
-                        <li class="">手机</li>
-                        <li class="">影音</li>
-                        <li class="">日杂</li>
-                        <li class="">服饰</li>
-                        <li class="">健康</li>
-                        <li class="">餐厨</li>
-                        <li class="">智能</li>
-                        <li class="">洗护</li>
-                        <li class="">箱包</li>
-                        <li class="">婴童</li>
-                        <li class="">饮食</li>
-                        <li class="">配件</li>
-                        <li class="">出行</li>
-                        <li class="">品牌</li>
-                    </ul>
-                </div>
-                <div class="content">
-                    <div class="banner">
-                        <img src="https://shop.io.mi-img.com/app/shop/img?id=shop_31a86d0b20837496b767b62133624fc1.jpeg&amp;w=786&amp;h=395"
-                            alt="">
-                    </div>
-                    <div class="items">
-                        <div class="item">
-                            <img src="https://shop.io.mi-img.com/app/shop/img?id=shop_3142c5002e8cd07300e478007e4ced9a.jpeg&amp;w=480&amp;h=480"
-                                alt="">
-                            <span>新品</span>
-                        </div>
-                        <div class="item"></div>
-                        <div class="item"></div>
-                    </div>
-                </div>
+        <div class="nav">
+                <ul>
+                    <li class="current" :type="type===0?'success':'info'" @click="type=0">有品推荐</li>
+                    <li class="" :type="type===1?'success':'info'" @click="type=1">居家</li>
+                    <li class="" :type="type===2?'success':'info'" @click="type=2">家电</li>
+                    <li class="" :type="type===3?'success':'info'" @click="type=3">手机</li>
+                    <li class="" :type="type===4?'success':'info'" @click="type=4">影音</li>
+                    <li class="" :type="type===5?'success':'info'" @click="type=5">日杂</li>
+                    <li class="" :type="type===6?'success':'info'" @click="type=6">服饰</li>
+                    <li class="" :type="type===7?'success':'info'" @click="type=7">健康</li>
+                    <li class="" :type="type===8?'success':'info'" @click="type=8">餐厨</li>
+                    <li class="" :type="type===9?'success':'info'" @click="type=9">智能</li>
+                    <li class="" :type="type===10?'success':'info'" @click="type=10">洗护</li>
+                    <li class="" :type="type===11?'success':'info'" @click="type=11">箱包</li>
+                    <li class="" :type="type===12?'success':'info'" @click="type=12">婴童</li>
+                    <li class="" :type="type===13?'success':'info'" @click="type=13">饮食</li>
+                    <li class="" :type="type===14?'success':'info'" @click="type=14">配件</li>
+                    <li class="" :type="type===15?'success':'info'" @click="type=15">出行</li>
+                    <li class="" :type="type===16?'success':'info'" @click="type=16">品牌</li>
+                </ul>
             </div>
+            <Recommend></Recommend>
+        </div>
 <Tab></Tab>
   </div>
 </template>
 
 <script>
 import Tab from '../../components/Tab';
+import Recommend from './Recommend';
 export default {
   components: {
-      Tab
+      Tab,
+      Recommend
   }
 }
 </script>
@@ -59,7 +57,42 @@ export default {
     padding-bottom: .51rem;
     overflow: hidden;
     background: #FFF;
-
+    .top{
+        box-sizing: border-box;
+        width: 3.2rem;
+        height: .4rem;
+        padding: .05rem .12rem;
+        border-bottom: .01rem solid #ccc;
+        
+        .inp{
+            width: 2.88rem;
+            height: .3rem;
+            line-height: .3rem;
+            border-radius: .32rem;
+            background-color: rgb(245, 245, 245);
+            .icon{
+                display:inline-block;
+                width: .17rem; 
+                height: .17rem;
+                line-height: .17rem;
+                padding-left: .16rem;
+                img{
+                    width: 100%; 
+                    height: 100%;
+                    vertical-align: bottom;
+                } 
+            }
+            span{
+                color: rgb(153, 153, 153);
+                padding:.02rem;
+                margin-left: .06rem;
+                color: rgb(153, 153, 153);
+                font-size: .12rem;
+            }
+        }
+       
+    }
+    
     .g-body {
         display: flex;
         height: 100%;
@@ -81,60 +114,17 @@ export default {
             li {
                 height: .24rem;
                 line-height: .24rem;
-                margin-bottom: .2rem;
                 white-space: nowrap;
-                font-size: .12rem;
+                font-size: .14rem;
+                margin:.1rem 0;
+                &.current{
 
-                &.current {
-                    font-size: .13rem;
-                    border-left: .04rem solid rgb(132, 95, 63);
-                    color: rgb(132, 95, 63);
+                    background: linear-gradient(to right, rgb(241, 83, 56), rgb(223, 20, 15));
+                    border-radius: 12px;
+                    margin: 0 .08rem;
+                    color: rgb(255,255,255);
                 }
-            }
-        }
-
-        .content {
-            padding: .1rem .06rem;
-            width: 75%;
-            height: 100%;
-            overflow-y: auto;
-
-            &::-webkit-scrollbar {
-                display: none;
-            }
-
-            .banner {
-                img {
-                    display: block;
-                    width: 100%;
-                    height: 100%;
-                }
-            }
-
-            .items {
-                display: flex;
-                flex-flow: row wrap;
-                justify-content: space-between;
-
-                .item {
-                    display: flex;
-                    flex-direction: column;
-                    flex: 0 0 26%;
-                    text-align: center;
-
-                    img {
-                        width: 100%;
-                    }
-
-                    span {
-                        font-size: .11rem;
-                        color: rgb(102, 102, 102);
-                    }
-
-                    &:nth-of-type(3n) {
-                        margin-right: 0;
-                    }
-                }
+                
             }
         }
     }
---
+++ xiaomi/src/pages/Classify/Recommend.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="content">
+        <div class="banner">
+            <img src="https://shop.io.mi-img.com/app/shop/img?id=shop_31a86d0b20837496b767b62133624fc1.jpeg&amp;w=786&amp;h=395" alt="">
+        </div>
+        <div class="items">
+            <div class="item">
+                <img src="https://shop.io.mi-img.com/app/shop/img?id=shop_3142c5002e8cd07300e478007e4ced9a.jpeg&amp;w=480&amp;h=480" alt="">
+                    <span>新品</span>
+            </div>
+            <div class="item"></div>
+            <div class="item"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.content {
+            padding: .1rem .06rem;
+            width: 75%;
+            height: 100%;
+            overflow-y: auto;
+
+            &::-webkit-scrollbar {
+                display: none;
+            }
+
+            .banner {
+                img {
+                    display: block;
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+
+            .items {
+                display: flex;
+                flex-flow: row wrap;
+                justify-content: space-between;
+
+                .item {
+                    display: flex;
+                    flex-direction: column;
+                    flex: 0 0 26%;
+                    text-align: center;
+
+                    img {
+                        width: 100%;
+                    }
+
+                    span {
+                        font-size: .11rem;
+                        color: rgb(102, 102, 102);
+                    }
+
+                    &:nth-of-type(3n) {
+                        margin-right: 0;
+                    }
+                }
+            }
+}
+</style>
