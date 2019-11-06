b5760fce0e6c637e5d8e2c4297a45d3ed46f97af
wenjunhui <841956130@qq.com>
2019-11-06 12:43:08 +0800

    两个页面

---
+++ xiaomi/src/components/Good.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="good">
+    <div class="left">
+      <img alt v-lazy="good.url" />
+    </div>
+    <div class="right">
+      <h3 v-text="good.title" class="title"></h3>
+      <p v-text="good.describe" class="describe"></p>
+      <p v-text="good.price?'人民币' +good.price + '元':null " class="pricee"></p>
+      <p v-text="good.gid?good.gid + '条评论':'暂无评论'" class="gid"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    good: {
+      type: Object,
+      default: function(){
+        return {}
+      },
+      required: true
+    }
+  },
+  data() {
+    return {
+      gd: {
+        gid: "890",
+        title: "全天候机能城市背包",
+        describe: "拒水性3-4级，背负升级",
+        url:
+          "https://shop.io.mi-img.com/app/shop/img?id=shop_4b6dfabbc1c80fa33df4189d1b323d26.jpeg",
+        link: "/detail?gid=890",
+        price: 189,
+        mark: true
+      }
+    };
+  },
+  components: {},
+  updated(){
+  }
+};
+</script>
+
+<style lang='less' scoped>
+.good {
+  box-sizing: border-box;
+  width: 100%;
+  height: 1.2rem;
+  .left {
+    float: left;
+    width: 30%;
+    height: 100%;
+    
+    img {
+      width: 100%;
+      height: 100%;
+      padding: .2rem;
+    }
+  }
+  .right {
+    float: right;
+    padding-left: 0.2rem;
+    width: 60%;
+  
+  }
+}
+</style>
--- xiaomi/src/pages/Detail/Detail.vue
+++ xiaomi/src/pages/Detail/Detail.vue
@@ -16,15 +16,127 @@
 <el-menu-item index="4">信用</el-menu-item>
 <el-menu-item index="5">筛选</el-menu-item>
      </el-menu> 
+   <Good :good='item' v-for="(item,index) in goods" :key="index"></Good>   
   </div>
 </template>
 
 <script>
+import Good from '../../components/Good';
 export default {
   data() {
     return {
       input2: "",
-      activeIndex: '1'
+      activeIndex: '1',
+      goods:[
+  {
+    "gid" : "890",
+    "title": "全天候机能城市背包",
+    "describe": "拒水性3-4级，背负升级",
+    "url": "https://shop.io.mi-img.com/app/shop/img?id=shop_4b6dfabbc1c80fa33df4189d1b323d26.jpeg",
+    "link":"/detail?gid=890",
+    "price": 189,
+    "mark":true
+  },
+  {
+    "gid" : "891",
+    "title": "时尚菱格双肩包",
+    "describe": "防泼水，背负舒适",
+    "url": "https://shop.io.mi-img.com/app/shop/img?id=shop_c8c20e8ad6a0e573679f1669fc68a99b.jpeg",
+    "link":"/detail?gid=891",
+    "price": 249,
+    "mark":false
+  },
+  {
+    "gid" : "655",
+    "title": "90分百搭运动折叠包",
+    "describe": "三重背法，轻便可折叠",
+    "url": "https://static.home.mi.com/app/shop/img?id=shop_f04e5b9bbd25c7aecb7fa396412192f3.jpeg",
+    "link":"/detail?gid=655",
+    "price": 49,
+    "mark":false
+  },
+  {
+    "gid" : "759",
+    "title": "都市简约双肩背包",
+    "describe": "防泼水，背负舒适",
+    "url": "https://static.home.mi.com/app/shop/img?id=shop_25c5118ca5d365eecee7d4b69fbfa068.jpeg",
+    "link":"/detail?gid=759",
+    "price": 169,
+    "mark":false
+  },
+  {
+    "gid" : "890",
+    "title": "全天候机能城市背包",
+    "describe": "拒水性3-4级，背负升级",
+    "url": "https://shop.io.mi-img.com/app/shop/img?id=shop_4b6dfabbc1c80fa33df4189d1b323d26.jpeg",
+    "link":"/detail?gid=890",
+    "price": 189,
+    "mark":true
+  },
+  {
+    "gid" : "891",
+    "title": "时尚菱格双肩包",
+    "describe": "防泼水，背负舒适",
+    "url": "https://shop.io.mi-img.com/app/shop/img?id=shop_c8c20e8ad6a0e573679f1669fc68a99b.jpeg",
+    "link":"/detail?gid=891",
+    "price": 249,
+    "mark":false
+  },
+  {
+    "gid" : "655",
+    "title": "90分百搭运动折叠包",
+    "describe": "三重背法，轻便可折叠",
+    "url": "https://static.home.mi.com/app/shop/img?id=shop_f04e5b9bbd25c7aecb7fa396412192f3.jpeg",
+    "link":"/detail?gid=655",
+    "price": 49,
+    "mark":false
+  },
+  {
+    "gid" : "759",
+    "title": "都市简约双肩背包",
+    "describe": "防泼水，背负舒适",
+    "url": "https://static.home.mi.com/app/shop/img?id=shop_25c5118ca5d365eecee7d4b69fbfa068.jpeg",
+    "link":"/detail?gid=759",
+    "price": 169,
+    "mark":false
+  },
+  {
+    "gid" : "890",
+    "title": "全天候机能城市背包",
+    "describe": "拒水性3-4级，背负升级",
+    "url": "https://shop.io.mi-img.com/app/shop/img?id=shop_4b6dfabbc1c80fa33df4189d1b323d26.jpeg",
+    "link":"/detail?gid=890",
+    "price": 189,
+    "mark":true
+  },
+  {
+    "gid" : "891",
+    "title": "时尚菱格双肩包",
+    "describe": "防泼水，背负舒适",
+    "url": "https://shop.io.mi-img.com/app/shop/img?id=shop_c8c20e8ad6a0e573679f1669fc68a99b.jpeg",
+    "link":"/detail?gid=891",
+    "price": 249,
+    "mark":false
+  },
+  {
+    "gid" : "655",
+    "title": "90分百搭运动折叠包",
+    "describe": "三重背法，轻便可折叠",
+    "url": "https://static.home.mi.com/app/shop/img?id=shop_f04e5b9bbd25c7aecb7fa396412192f3.jpeg",
+    "link":"/detail?gid=655",
+    "price": 49,
+    "mark":false
+  },
+  {
+    "gid" : "759",
+    "title": "都市简约双肩背包",
+    "describe": "防泼水，背负舒适",
+    "url": "https://static.home.mi.com/app/shop/img?id=shop_25c5118ca5d365eecee7d4b69fbfa068.jpeg",
+    "link":"/detail?gid=759",
+    "price": 169,
+    "mark":false
+  }
+]
     };
   },
   methods:{
@@ -38,7 +150,9 @@ this.$router.push({path:'/home'});
   created() {
     this.input2 = this.$route.query.title;
   },
-  components: {}
+  components: {
+    Good
+  }
 };
 </script>
 
