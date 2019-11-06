fbbefce77cc7cb3fae5016ccb936a3efe3850548
wenjunhui <841956130@qq.com>
2019-11-06 19:13:04 +0800

    两个页面

---
+++ xiaomi/src/components/Banner1.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <swiper-slide v-for="(slide, index) in swiperSlides" :key="index">
+        <img :src="slide.url" alt />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+   <div class="cart">
+   </div>
+  </div>
+</template>
+
+<script>
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+export default {
+  data() {
+    return {
+      swiperOption: {
+        autoplay: {
+          disableOnInteraction: false, //用户操作后是否禁止自动循环
+          delay: 500 //自自动循环时间
+        }, //可选选项，自动滑动
+        speed: 1000, //切换速度，即slider自动滑动开始到结束的时间（单位ms）
+        loop: true, //循环切换
+        grabCursor: true, //设置为true时，鼠标覆盖Swiper时指针会变成手掌形状，拖动时指针会变成抓手形状
+        // setWrapperSize: true, //Swiper使用flexbox布局(display: flex)，开启这个设定会在Wrapper上添加等于slides相加的宽或高，在对flexbox布局的支持不是很好的浏览器中可能需要用到。
+        autoHeight: true, //自动高度。设置为true时，wrapper和container会随着当前slide的高度而发生变化。
+        scrollbar: ".swiper-scrollbar",
+        mousewheelControl: true, //设置为true时，能使用鼠标滚轮控制slide滑动
+        observeParents: true, //当改变swiper的样式（例如隐藏/显示）或者修改swiper的子元素时，自动初始化swiper
+        pagination: {
+          el: ".swiper-pagination",
+          // type : 'progressbar', //分页器形状
+          clickable: true //点击分页器的指示点分页器会控制Swiper切换
+        }
+      },
+      swiperSlides: [
+        {
+          url:
+            "https://shop.io.mi-img.com/app/shop/img?id=shop_9334c3fd5811978f84fe7035ae54b00d.jpeg&w=1080&h=450&crop=a_0_0_1080_480&t=webp",
+          link: "https://home.mi.com/app/shopv3/site/0118"
+        },
+        {
+          url:
+            "https://shop.io.mi-img.com/app/shop/img?id=shop_800b78d4c2287c6c7a3681076f9b1606.jpeg&w=1080&h=450&crop=a_0_0_1080_480&t=webp",
+          link: "https://home.mi.com/detail?gid=100400"
+        },
+        {
+          url:
+            "https://shop.io.mi-img.com/app/shop/img?id=shop_a1e493684d4b9a60d2c4445e95960c81.jpeg&w=1080&h=450&crop=a_0_0_1080_480&t=webp",
+          link: "https://home.mi.com/app/shop/content?id=q283ad0390a96ed83"
+        },
+        {
+          url:
+            "https://shop.io.mi-img.com/app/shop/img?id=shop_7f79014b3984f9a2ca0ccd989fee960a.jpeg&w=1080&h=450&crop=a_0_0_1080_480&t=webp",
+          link: "https://home.mi.com/app/shop/content?id=s8fa0d039077a9c85"
+        },
+        {
+          url:
+            "https://shop.io.mi-img.com/app/shop/img?id=shop_940e96da0d8707e657c58c5d7278e64a.jpeg&w=1080&h=450&crop=a_0_0_1080_480&t=webp",
+          link: "https://home.mi.com/detail?gid=100982"
+        }
+      ],
+    
+
+
+    };
+  },
+  components: {
+    swiper,
+    swiperSlide
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper;
+    }
+  },
+  mounted() {}
+};
+</script>
+
+<style lang='less' scoped>
+.container {
+  width: 100%;
+  .swiper-container {
+    width: 100%;
+    .swiper-wrapper {
+        height: 1rem ;
+        .swiper-slide{
+            height: 2rem;
+            img{
+                width: 100%;
+                height: 100%;
+            }
+        }
+    }
+  }
+
+}
+</style>
--- xiaomi/src/pages/Cart/Cart.vue
+++ xiaomi/src/pages/Cart/Cart.vue
@@ -1,18 +1,108 @@
 <template>
-  <div>
-    购物车
+  <div class="cart">
+    <Banner1></Banner1>
+    <CartGoods :cartgood="item" v-for="(item,index) in cartgoods" :key="index+'cartgoods'"></CartGoods>
+    <h2  >为你推荐</h2>
+    <Good :good="item" v-for="(item,index) in goods" :key="index"></Good>
     <Tab></Tab>
   </div>
 </template>
 
 <script>
-import Tab from '../../components/Tab';
+import Banner1 from "../../components/Banner1";
+import Tab from "../../components/Tab";
+import Good from "../../components/Good";
+import CartGoods from '../../components/CartGoods';
 export default {
+  data() {
+    return {
+    cartgoods: [
+        {
+          gid: "891",
+          title: "时尚菱格双肩包",
+          describe: "防泼水，背负舒适",
+          url:
+            "https://shop.io.mi-img.com/app/shop/img?id=shop_c8c20e8ad6a0e573679f1669fc68a99b.jpeg",
+          link: "/detail?gid=891",
+          price: 249,
+          mark: false
+        },
+        {
+          gid: "655",
+          title: "90分百搭运动折叠包",
+          describe: "三重背法，轻便可折叠",
+          url:
+            "https://static.home.mi.com/app/shop/img?id=shop_f04e5b9bbd25c7aecb7fa396412192f3.jpeg",
+          link: "/detail?gid=655",
+          price: 49,
+          mark: false
+        }
+      ],
+      goods: [
+        {
+          gid: "890",
+          title: "全天候机能城市背包",
+          describe: "拒水性3-4级，背负升级",
+          url:
+            "https://shop.io.mi-img.com/app/shop/img?id=shop_4b6dfabbc1c80fa33df4189d1b323d26.jpeg",
+          link: "/detail?gid=890",
+          price: 189,
+          mark: true
+        },
+        {
+          gid: "891",
+          title: "时尚菱格双肩包",
+          describe: "防泼水，背负舒适",
+          url:
+            "https://shop.io.mi-img.com/app/shop/img?id=shop_c8c20e8ad6a0e573679f1669fc68a99b.jpeg",
+          link: "/detail?gid=891",
+          price: 249,
+          mark: false
+        },
+        {
+          gid: "655",
+          title: "90分百搭运动折叠包",
+          describe: "三重背法，轻便可折叠",
+          url:
+            "https://static.home.mi.com/app/shop/img?id=shop_f04e5b9bbd25c7aecb7fa396412192f3.jpeg",
+          link: "/detail?gid=655",
+          price: 49,
+          mark: false
+        },
+        {
+          gid: "759",
+          title: "都市简约双肩背包",
+          describe: "防泼水，背负舒适",
+          url:
+            "https://static.home.mi.com/app/shop/img?id=shop_25c5118ca5d365eecee7d4b69fbfa068.jpeg",
+          link: "/detail?gid=759",
+          price: 169,
+          mark: false
+        }
+      ]
+    };
+  },
   components: {
-    Tab
+    Tab,
+    Banner1,
+    Good,
+    CartGoods
   }
-}
+};
 </script>
 
 <style lang='less' scoped>
+.cart{
+  width: 100%;
+  h2{
+    width: 100%;
+    height: 1rem;
+    color: coral;
+    text-align: center;
+    line-height: 1rem;
+
+  }
+}
+
+
 </style>
